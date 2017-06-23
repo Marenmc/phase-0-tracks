7a2beb35b32297d5e10bb7281d63aa9c4f33c2dd
Maren McMullan <marenmc@gmail.com>
2017-06-22 21:47:40 -0700

    Adds vampires.rb file with 4 basic questions.

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+#Program to ensure new employees are not vampires
+
+puts "What is your name?"
+  name = gets.chomp
+
+puts "How old are you? What year were you born?"
+  age_year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Can I get you some?"
+  bread_order = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+  enrollment = gets.chomp
+
+
