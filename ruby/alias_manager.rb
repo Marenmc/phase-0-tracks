5e1f1f5965f1337594bb22e8555579bc5e87ce7b
Maren McMullan <marenmc@gmail.com>
2017-07-12 08:49:53 -0700

    Updates alias manger to iterate over vowels

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,29 +1,20 @@
 #Create interface to ask for a name
 puts "What's your full name? Split your first name and last name with a space"
-
-#Create a name variable
-name = gets.chomp
+name = gets.chomp!
 
 puts "Here is your name reversed"
-#Split the first name and last name
-#Reverse the first and last name
-#Create a new variable with the reversed name
-reversed_name = full_name.split.reverse.join('')
-p revesed_name
+reversed_name = name.split.reverse.join('')
+p reversed_name
 
 puts "and the letters split into an array"
 #Make a new string with the characters in the reversed name
 name_letters = reversed_name.split(//)
 p name_letters
 
-
-#Take a spy's name and turn a name
-name = "marenz"
-vowels = "aeiou"
-
 new_name = ""
 
 #Is the letter a vowel or consonant
+vowels = "aeiou"
 consonants = ("a".."z").to_a
 consonants.delete_if{ |letter| vowels.include? letter}
 
@@ -37,12 +28,12 @@ def nextLetter(letters, letter)
   letters[letterIndex]
 end
 
-name.each_char do |letter|
+reversed_name.each_char do |letter|
   if vowels.include? letter
     new_name += nextLetter(vowels, letter)
   elsif consonants.include? letter
     new_name += nextLetter(consonants, letter)
-  endc
+  end
 end
 puts new_name
 
