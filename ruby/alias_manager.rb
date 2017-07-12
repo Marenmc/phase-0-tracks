5314723117d14c39696a7a2509c9e52cbdc693c5
Maren McMullan <marenmc@gmail.com>
2017-07-12 08:43:53 -0700

    Adds updated code that actually iterates through vowels

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,8 +1,8 @@
 #Create interface to ask for a name
-puts "What's your full name?"
+puts "What's your full name? Split your first name and last name with a space"
 
 #Create a name variable
-full_name = gets.chomp
+name = gets.chomp
 
 puts "Here is your name reversed"
 #Split the first name and last name
@@ -17,60 +17,32 @@ name_letters = reversed_name.split(//)
 p name_letters
 
 
-#Create a method that iterates through the letters in the name_letters string.
-#If there is a vowel, change that vowel to vowel.next
-def character_translator(char)
-  vowels = "aeiou"
-  if char == "a"
-    "leep"
+#Take a spy's name and turn a name
+name = "marenz"
+vowels = "aeiou"
+
+new_name = ""
+
+#Is the letter a vowel or consonant
+consonants = ("a".."z").to_a
+consonants.delete_if{ |letter| vowels.include? letter}
+
+def nextLetter(letters, letter)
+  letterIndex = letters.index(letter)
+  if letterIndex == letters.length-1
+    letterIndex = 0
   else
-    " "
+    letterIndex = letterIndex+1
   end
-  char
+  letters[letterIndex]
 end
 
-def vowel_translator(full_name)
-  char_index = 0
-  translated_name = ""
-  while char_index < full_name.length
-   translated_name << character_translator(full_name[char_index])
-   char_index += 1
-  end
- translated_name << ""
+name.each_char do |letter|
+  if vowels.include? letter
+    new_name += nextLetter(vowels, letter)
+  elsif consonants.include? letter
+    new_name += nextLetter(consonants, letter)
+  endc
 end
-
-puts vowel_translator("Maren McMullan")
-puts character_translator("Maren McMullan")
-
-
-fake_name_data {
-  :name => (full_name)
- # :translated_name => (translated_name)
-}
-
-p fake_name_data
-
-
-
-#scratch pad below...
-
-# def vowel_switch(name_letters, vowels)
-#   #Keep iterating as long as the index number in the string is less than the length of the string
-#   while index < name_letters.length
-#   #If the letter is not a vowel, return the same letter
-#   #If the letter IS a vowel, return the vowel.next
-#   return index
-#     if name_letters.char == vowels
-#       puts vowels.next
-#     else puts " "
-#   end
-# end
-# end
-
-
-
-
-#   :consonants = "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"
-# }
-
+puts new_name
 
