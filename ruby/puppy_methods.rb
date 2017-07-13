8d6fb89f957e2cf5afd4441584332a42a8375349
Maren McMullan <marenmc@gmail.com>
2017-07-13 12:19:46 -0700

    Updates puppy methods to be able to iterate over each user in the same each method.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,59 +1,59 @@
-# ## RELEASE 0 AND 1 - PUPPIES
-# class Puppy
-#   #fetch method
-#   def fetch(toy)
-#     p "I brought back the #{toy}"
-#   end
+puts "----------RELEASE 0 AND 1 - PUPPIES-----------"
+class Puppy
+  #fetch method
+  def fetch(toy)
+    p "I brought back the #{toy}"
+  end
 
-#   #speak method
-#   def speak
-#     4.times{puts "Woof!"}
-#   end
+  #speak method
+  def speak
+    4.times{puts "Woof!"}
+  end
 
-#   #roll over method
-#   def roll_over
-#     p "*rolls over*"
-#   end
+  #roll over method
+  def roll_over
+    p "*rolls over*"
+  end
 
-#   #dog years method
-#   def dog_years(human_years)
-#     p 7*(human_years)
-#   end
+  #dog years method
+  def dog_years(human_years)
+    p 7*(human_years)
+  end
 
-#   #sit method
-#   def sit
-#     p "*sits*"
-#   end
+  #sit method
+  def sit
+    p "*sits*"
+  end
 
-#   #initialize new Puppy
-#   def initialize
-#     puts "Initializing a new puppy instance"
-#   end
+  #initialize new Puppy
+  def initialize
+    puts "Initializing a new puppy instance"
+  end
 
-# end
+end
 
-# #Initialize a new instance of a Puppy
-# pepper = Puppy.new
+#Initialize a new instance of a Puppy
+pepper = Puppy.new
 
-# #Get new puppy to fetch a ball
-# pepper.fetch("ball")
+#Get new puppy to fetch a ball
+pepper.fetch("ball")
 
-# #Get new puppy to speak
-# pepper.speak
+#Get new puppy to speak
+pepper.speak
 
-# #Get puppy to roll over
-# pepper.roll_over
+#Get puppy to roll over
+pepper.roll_over
 
-# #Get puppy age in dog years
-# pepper.dog_years(2)
+#Get puppy age in dog years
+pepper.dog_years(2)
 
-# #Get puppy to sit
-# pepper.sit
+#Get puppy to sit
+pepper.sit
 
-# #Run the Initializing instance
-# Puppy.new
+#Run the Initializing instance
+Puppy.new
 
-## RELEASE 2 - eSpark student
+puts "----------RELEASE 2 - NEW STUDENT-----------"
 
 class Student
   #Initialize new student
@@ -74,24 +74,18 @@ end
 
 student_array = []
 
-# #Use a loop to create 50 instances of our data
-while student_array.length < 5
+#Use a loop to create 50 instances of our data
+while student_array.length < 51
 student_array << student = Student.new
 end
 
-student_array.each {|item| puts item.test_scores("A")}
-student_array.each {|item| puts item.go_to_recess}
+#Iterate over each of the 50 user
+student_array.each do |item|
+  Student.new
+  puts item.test_scores("A")
+  puts item.go_to_recess
+end
 
-p student_array
-
-# student_array.each do
-#   while student_array.length < 51
-#   student_array << student = Student.new
-#   test_scores("A+")
-#   go_to_recess
-#   end
-# end
-
-
+## Question for code review - why is there a space between recess and test scores??
 
 
