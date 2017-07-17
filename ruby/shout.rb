36742daaa7090011f9d2669678b67dd0012a51ea
Maren McMullan <marenmc@gmail.com>
2017-07-16 18:28:26 -0700

    Updates shout file to include a module. Also updates flight.rb

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,43 @@
+##NEW CODE
+
 module Shout
- def self.yell_angrily(words)
-    words + "!!!" + " :("
+ def yell_angrily(words)
+    puts words + "!!!" + " :("
  end
 
- def self.yell_happily(words)
-     words + "Whooo!" + "Fuck yeah!"
+ def yell_happily(words)
+    puts words + " Whooo!" + " Fuck yeah!"
  end
 end
 
-#driver code
-p Shout.yell_angrily("oh no you didn't")
-p Shout.yell_happily("I won a million dollars!")
+class Moms
+  include Shout
+end
+
+class Coaches
+  include Shout
+end
+
+mom = Moms.new
+mom.yell_angrily("Maren Elizabeth McMullan!")
+
+coach = Coaches.new
+coach.yell_happily("Go team!")
+
+
+
+
+##OLD CODE
+# module Shout
+#  def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#  end
+
+#  def self.yell_happily(words)
+#      words + "Whooo!" + "Fuck yeah!"
+#  end
+# end
+
+# #driver code
+# p Shout.yell_angrily("oh no you didn't")
+# p Shout.yell_happily("I won a million dollars!")
