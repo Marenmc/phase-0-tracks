f6579867f8a961a8933814c2f0a3e866a808106d
Maren McMullan <marenmc@gmail.com>
2017-07-25 15:03:54 -0500

    Updates data strucutres to include creating several cars

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,6 +1,8 @@
+// MY HORSES - RELEASES 0 AND 1
+
 // two separate arrays
-
-
+// var colors = ["periwinkle", "cyan", "raspberry", "marigold"];
+// var names = ["Chocolate Chip", "Beauty", "Fasty", "Lickety Split"];
 
 // console.log(colors);
 // colors.push("charcoal");
@@ -13,18 +15,30 @@
 // declare an empty object
 // Create a loop that sets the index to one, specifies how many times to loop through, accesses the array, pushes the data in the array to the obect, and then iterates to the next item in the index
 
-var colors = ["periwinkle", "cyan", "raspberry", "marigold"];
-var names = ["Chocolate Chip", "Beauty", "Fasty", "Lickety Split"];
 
-function addHorses(names, colors){
-  var horses = {};
-  for (var i = 0; i < colors.length; i++) {
-    horses[names[i]] = colors[i];
-  }
-  return horses
+// function addHorses(names, colors){
+//   var horses = {};
+//   for (var i = 0; i < colors.length; i++) {
+//     horses[names[i]] = colors[i];
+//   }
+//   return horses
+// }
+
+// console.log(addHorses(names, colors))
+
+// MAKING CARS - RELEASE 3
+
+function Car(make, model, year, isNew) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.isNew = isNew;
 }
 
-console.log(addHorses(names, colors))
-
-
-
+var marensCar = new Car("honda", "fit", 2011, true);
+console.log("Maren's car is a " + marensCar['year'] + " " + marensCar['make'] + " " + marensCar['model']  + ". Is her car new? " + marensCar['isNew'])
+console.log(marensCar)
+var marysCar = new Car("honda", "crv", 2014, true);
+console.log(marysCar)
+var momsCar = new Car("BMW", "5 series", 2010, false);
+console.log(momsCar)
